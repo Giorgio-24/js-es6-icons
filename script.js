655d39fdcdb9d9f42013674346e4e5755b1c32af
Giorgio Andrei <cipuandrei@libero.it>
2021-07-26 17:07:14 +0200

    cards-generated-in-page

---
+++ script.js
@@ -0,0 +1,17 @@
+const display = document.getElementById("cards-grid");
+
+let cardsTemplate = '';
+
+icons.forEach((icons) => {
+    cardsTemplate += `
+    <div class="d-inline-block col-2 card">
+      <div class="card-body">
+    <i class="${icons.family} ${icons.prefix}${icons.name} fa-2x"></i>
+    <h2 class="h6">${icons.name.toUpperCase()}</h2>
+      </div>
+    </div>`;
+});
+
+display.innerHTML = cardsTemplate;
+/*  class="${icons.family} ${icons.prefix}${icons.name}" */
+
