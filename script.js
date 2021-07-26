5991948a01e869c391bb61f4b374519f5ac20f68
Giorgio Andrei <cipuandrei@libero.it>
2021-07-26 17:12:13 +0200

    icon-colors-added-to-each-icon-type

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ icons.forEach((icons) => {
     cardsTemplate += `
     <div class="d-inline-block col-2 card">
       <div class="card-body">
-    <i class="${icons.family} ${icons.prefix}${icons.name} fa-2x"></i>
+    <i class="${icons.family} ${icons.prefix}${icons.name} fa-2x ${icons.type}"></i>
     <h2 class="h6">${icons.name.toUpperCase()}</h2>
       </div>
     </div>`;
